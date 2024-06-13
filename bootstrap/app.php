eef2280e8a2acd74c321f798853b1c862752c332
mhsdaniel <sulthonak13@gmail.com>
2024-06-13 13:02:49 +0700

    menambahkan comments

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,9 +5,9 @@
 | Create The Application
 |--------------------------------------------------------------------------
 |
-| The first thing we will do is create a new Laravel application instance
-| which serves as the "glue" for all the components of Laravel, and is
-| the IoC container for the system binding all of the various parts.
+| Hal pertama yang akan kita lakukan adalah membuat instance aplikasi Laravel baru
+| yang berfungsi sebagai "perekat" untuk semua komponen Laravel, dan merupakan
+| wadah IoC untuk sistem yang mengikat semua bagian.
 |
 */
 
@@ -20,9 +20,9 @@ $app = new Illuminate\Foundation\Application(
 | Bind Important Interfaces
 |--------------------------------------------------------------------------
 |
-| Next, we need to bind some important interfaces into the container so
-| we will be able to resolve them when needed. The kernels serve the
-| incoming requests to this application from both the web and CLI.
+|Selanjutnya, kita perlu mengikat beberapa antarmuka penting ke dalam wadah
+| kami akan dapat menyelesaikannya bila diperlukan. Kernel melayani
+| permintaan masuk ke aplikasi ini dari web dan CLI.
 |
 */
 
@@ -46,9 +46,9 @@ $app->singleton(
 | Return The Application
 |--------------------------------------------------------------------------
 |
-| This script returns the application instance. The instance is given to
-| the calling script so we can separate the building of the instances
-| from the actual running of the application and sending responses.
+| Skrip ini mengembalikan contoh aplikasi. Contohnya diberikan kepada
+| skrip pemanggil sehingga kita dapat memisahkan pembuatan instance
+| dari menjalankan aplikasi secara aktual dan mengirimkan respons.
 |
 */
 
